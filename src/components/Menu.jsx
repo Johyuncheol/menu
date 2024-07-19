aec662d137ae93b4bf7ad59001e21a341d52ffda
조현철 <finos3703@naver.com>
2024-07-19 16:29:35 +0900

    first

---
+++ src/components/Menu.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { useState } from "react";
+import styles from "./Menu.module.css";
+
+const Menu = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [Toggle, setToggle] = useState(true);
+
+  const ClickToggleHandler = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <ul className={`${styles.MenuUl}`}>
+      <li className={styles.EndEnd} onClick={() => ClickToggleHandler()}>
+        <span>첫 제목</span>
+        <span>{Toggle ? (isOpen ? "v" : "<") : ""}</span>
+      </li>
+      {Toggle && isOpen && (
+        <>
+          <li>하위 제목</li>
+          <li>하위 제목</li>
+          <li>하위 제목</li>
+          <li>하위 제목</li>
+          <li>하위 제목</li>
+          <li>하위 제목</li>
+          <li>하위 제목</li>
+        </>
+      )}
+    </ul>
+  );
+};
+
+export default Menu;
