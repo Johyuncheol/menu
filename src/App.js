614839d849f7ee8d1c61f6da81c0980da0816550
조현철 <finos3703@naver.com>
2024-07-19 17:59:26 +0900

    qwe

--- src/App.js
+++ src/App.js
@@ -13,35 +13,34 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <main className="main">
-        {/* 사이드바  */}
-        <aside className="sidebar">
-          <Profile />
-          <div className="menuLable">관리자 메뉴</div>
-          <div className="adminMenu">
-            <Menu />
-          </div>
-          <div className="menuLable">메뉴</div>
-          <div className="Menu">
-            <Menu />
-          </div>
-        </aside>
 
-        {/* 메인  */}
-        <section >
-          메뉴 추가하기
-          <form className="form">
-            <label>메뉴 이름</label>
-            <input type="text" />
+      {/* 사이드바  */}
+      <aside className="sidebar">
+        <Profile />
+        <div className="menuLable">관리자 메뉴</div>
+        <div className="adminMenu">
+          <Menu />
+        </div>
+        <div className="menuLable">메뉴</div>
+        <div className="Menu">
+          <Menu />
+        </div>
+      </aside>
 
-            <label>카테고리</label>
-            <select>
-              <option>111</option>
-              <option>222</option>
-            </select>
-          </form>
-        </section>
-      </main>
+      {/* 메인  */}
+      <section className="main">
+        메뉴 추가하기
+        <form className="form">
+          <label>메뉴 이름</label>
+          <input type="text" />
+
+          <label>카테고리</label>
+          <select>
+            <option>111</option>
+            <option>222</option>
+          </select>
+        </form>
+      </section>
     </div>
   );
 }
