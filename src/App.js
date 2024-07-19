a476fca0ad0a0edfb608613722c4fd5a73d6a03b
조현철 <finos3703@naver.com>
2024-07-19 17:54:41 +0900

    qwewe

--- src/App.js
+++ src/App.js
@@ -2,22 +2,46 @@ import "./App.css";
 import Menu from "./components/menu/Menu";
 import Header from "./components/header/Header";
 import Profile from "./components/profile/Profile";
+import {
+  DragDropContext,
+  Draggable,
+  Droppable,
+  DropResult,
+} from "react-beautiful-dnd";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <aside className="sidebar">
-        <Profile />
-        <div className="menuLable">관리자 메뉴</div>
-        <div className="adminMenu">
-          <Menu />
-        </div>
-        <div className="menuLable">메뉴</div>
-        <div className="Menu">
-          <Menu />
-        </div>
-      </aside>
+      <main className="main">
+        {/* 사이드바  */}
+        <aside className="sidebar">
+          <Profile />
+          <div className="menuLable">관리자 메뉴</div>
+          <div className="adminMenu">
+            <Menu />
+          </div>
+          <div className="menuLable">메뉴</div>
+          <div className="Menu">
+            <Menu />
+          </div>
+        </aside>
+
+        {/* 메인  */}
+        <section >
+          메뉴 추가하기
+          <form className="form">
+            <label>메뉴 이름</label>
+            <input type="text" />
+
+            <label>카테고리</label>
+            <select>
+              <option>111</option>
+              <option>222</option>
+            </select>
+          </form>
+        </section>
+      </main>
     </div>
   );
 }
